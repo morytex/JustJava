409ad9d67b21087b318210de351443794f39a4de
moryta_cit <moryta@ciandt.com>
2016-08-30 20:41:06 -0300

    Implementing feature to order coffee
    Increment order and price count as we click on button

--- app/src/main/java/com/example/moryta/justtava/MainActivity.java
+++ app/src/main/java/com/example/moryta/justtava/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.moryta.justtava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
-
+    private int order;
+    private int price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        order = 0;
+        price = 2;
         setContentView(R.layout.activity_main);
+        display(order);
+        displayPrice(order * price);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(++order);
+        displayPrice(order * price);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
